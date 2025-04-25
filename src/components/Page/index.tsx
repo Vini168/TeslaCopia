46e1c6f4a9a4389a2aa66779a24c40b5bbf6502c
Vinicius <vinicius000168@gmail.com>
2025-04-25 19:17:20 -0300

    first commit

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { ModelsWrapper, ModelSection } from '../Model'
+import DefaultOverlayContent from '../DefaultOverlayContent'
+import UniqueOverlay from '../UniqueOverlay'
+
+import { Container, Spacer } from './styles'
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      <ModelsWrapper>
+        <div>
+          {[
+            'Model Y',
+            'CyberTruck',
+            'Model 3',
+            'Model S',
+            'Solar Panels',
+            'Solar Roof',
+            'Acessories'
+          ].map(modelName => (
+            <ModelSection
+              key={modelName}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+          ))}
+        </div>
+
+        <Spacer />
+
+        <UniqueOverlay />
+      </ModelsWrapper>
+    </Container>
+  )
+}
+
+export default Page
---
+++ src/components/Page/styles.ts
@@ -0,0 +1,44 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  .colored:nth-child(1) {
+     background-image: url('/images/tesla01.png');
+     background-size: cover;
+     background-position: center;
+  }
+  .colored:nth-child(2) {
+    background-image: url('/images/tesla02.png');
+     background-size: cover;
+     background-position: center;
+  }
+  .colored:nth-child(3) {
+    background-image: url('/images/tesla03.png');
+     background-size: cover;
+     background-position: center;
+  }
+  .colored:nth-child(4) {
+    background-image: url('/images/tesla04.png');
+     background-size: cover;
+     background-position: center;
+  }
+  .colored:nth-child(5) {
+    background-image: url('/images/tesla05.png');
+     background-size: cover;
+     background-position: center;
+  }
+  .colored:nth-child(6) {
+    background-image: url('/images/tesla06.png');
+     background-size: cover;
+     background-position: center;
+  }
+  .colored:nth-child(7) {
+    background-image: url('/images/tesla07.png');
+     background-size: cover;
+     background-position: center;
+  }
+`
+
+export const Spacer = styled.div`
+  height: 15vh;
+  background:rgb(0, 0, 0);
+`
